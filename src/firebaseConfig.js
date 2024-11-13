c95f178a5cb3ded251212eb0e50287ba4b1dec3d
Barkın Özkaptan <72075883+bbbarkoo@users.noreply.github.com>
2024-11-14 00:06:13 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,14 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWhhiWVhFaXyQg2zArMcPW2gK6J5GyTzM",
-  authDomain: "vbucoursesystem.firebaseapp.com",
-  projectId: "vbucoursesystem",
-  databaseURL:"https://vbucoursesystem-default-rtdb.europe-west1.firebasedatabase.app/",
-  storageBucket: "vbucoursesystem.appspot.com",
-  messagingSenderId: "176805141629",
-  appId: "1:176805141629:web:d3022e1d3617a8d0e6a514",
-  measurementId: "G-DK9YN9Q95G"
+ 
 };
 
 // Initialize Firebase
